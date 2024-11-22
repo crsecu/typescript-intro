7ee88fa473ade2ae83976fc7ccf9bb3931fdbe58
Cristina Secu <secu.cristina@yahoo.com>
2024-11-22 13:48:47 -0500

    TS practice

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -2,19 +2,19 @@ interface User {
   id: number;
   firstName: string;
   lastName: string;
+  role: "admin" | "user" | "super-admin";
   /**
    * How do we ensure that role is only one of:
    * - 'admin'
    * - 'user'
    * - 'super-admin'
    */
-  role: string;
 }
 
 export const defaultUser: User = {
   id: 1,
   firstName: "Matt",
   lastName: "Pocock",
-  // @ts-expect-error
-  role: "I_SHOULD_NOT_BE_ALLOWED",
+
+  role: "admin",
 };
